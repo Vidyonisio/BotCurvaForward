e23a2b122720e0fde8b81fea53a96bc423ec37a1
Vidyonisio <vidyonisio@gmail.com>
2022-08-11 13:39:40 -0300

    Versão 1.0

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pyautogui
 import time
 import datetime
 
-"curva na tela da esquerda opera com scroll no topo: Point(x=328, y=316)"
+"curva na tela da esquerda opera com scroll no topo: Point(x=306, y=316)"
 "Botão Consultar data: Point(x=548, y=315)"
 "Botão Exportar p/ excel: Point(x=1262, y=352)"
 
@@ -24,20 +24,24 @@ def str_lista(d,m,y):
     return (string_lista)
 
 def bot(data):
-    pyautogui.click(328,316)
-    pyautogui.click(328,316)
+    pyautogui.moveTo(306,316)
+    pyautogui.click()
     pyautogui.doubleClick()
+    time.sleep(0.1)
     pyautogui.typewrite(data)
+    time.sleep(0.1)
     pyautogui.click(548,315)
     time.sleep(1.2)  #1.2
-    pyautogui.click(1262,352)
+    pyautogui.moveTo(1262,352)
+    time.sleep(0.08)
+    pyautogui.click()
 
 for i in range(d_mes-day1+1):
     data_str=str_lista(day, month, year)
     print(data_str)
     bot(data_str)
     day +=1
-    time.sleep(0.7) #0.5
+    time.sleep(1.6) #1
 
 
 
